df51c29081140992a0b6ae6bf16fa0370696b754
Kcrunchy <79080612+Kcrunchy@users.noreply.github.com>
2021-06-01 21:20:44 +0300

    Add files via upload

---
+++ Video tutorial pinger.bat
@@ -0,0 +1,114 @@
+@echo off
+chcp 65001 >nul
+:login
+cls
+color 2
+title *video tutorial*
+echo 
+echo ██╗      ██████╗  ██████╗ ██╗███╗   ██╗
+echo ██║     ██╔═══██╗██╔════╝ ██║████╗  ██║
+echo ██║     ██║   ██║██║  ███╗██║██╔██╗ ██║
+echo ██║     ██║   ██║██║   ██║██║██║╚██╗██║
+echo ███████╗╚██████╔╝╚██████╔╝██║██║ ╚████║
+echo ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝╚═╝  ╚═══╝
+echo                                        
+echo 
+set /p user=Username:
+echo.
+set /p pass=Password:
+if %user% == 123 if %pass% == 321 goto main
+echo try again *Wait 5 Senconds*
+timeout 5 >nul
+goto login
+:main
+@echo off
+chcp 65001
+title DRFV PINGER
+color E
+
+
+@echo  ██▒   █▓ ██▓▓█████▄    ▄▄▄█████▓ █    ██ ▄▄▄█████▓ ▒█████   ██▀███   ██▓ ▄▄▄       ██▓    
+@echo ▓██░   █▒▓██▒▒██▀ ██▌   ▓  ██▒ ▓▒ ██  ▓██▒▓  ██▒ ▓▒▒██▒  ██▒▓██ ▒ ██▒▓██▒▒████▄    ▓██▒    
+@echo  ▓██  █▒░▒██▒░██   █▌   ▒ ▓██░ ▒░▓██  ▒██░▒ ▓██░ ▒░▒██░  ██▒▓██ ░▄█ ▒▒██▒▒██  ▀█▄  ▒██░    
+@echo   ▒██ █░░░██░░▓█▄   ▌   ░ ▓██▓ ░ ▓▓█  ░██░░ ▓██▓ ░ ▒██   ██░▒██▀▀█▄  ░██░░██▄▄▄▄██ ▒██░    
+@echo   ▒▀█░  ░██░░▒████▓      ▒██▒ ░ ▒▒█████▓   ▒██▒ ░ ░ ████▓▒░░██▓ ▒██▒░██░ ▓█   ▓██▒░██████▒
+@echo    ░ ▐░  ░▓   ▒▒▓  ▒      ▒ ░░   ░▒▓▒ ▒ ▒   ▒ ░░   ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░░▓   ▒▒   ▓▒█░░ ▒░▓  ░
+@echo    ░ ░░   ▒ ░ ░ ▒  ▒        ░    ░░▒░ ░ ░     ░      ░ ▒ ▒░   ░▒ ░ ▒░ ▒ ░  ▒   ▒▒ ░░ ░ ▒  ░
+@echo      ░░   ▒ ░ ░ ░  ░      ░       ░░░ ░ ░   ░      ░ ░ ░ ▒    ░░   ░  ▒ ░  ░   ▒     ░ ░   
+@echo       ░   ░     ░                   ░                  ░ ░     ░      ░        ░  ░    ░  ░
+@echo      ░        ░                                                                            
+
+
+@echo -------------------------------------------   
+@echo video tutorial pinger
+@echo for skids
+@echo Made by ur mom
+@echo -------------------------------------------   
+
+set /p IP=Enter kid ip:
+:rainbow
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+
+color a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 02
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 03
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 04
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 05
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 06
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 07
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 08
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 09
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0a
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0b
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0c
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...) 
+ping -t 2 0 10 127.0.0.1 >nul
+color 0d
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0e
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+color 0f
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=c & echo offline...)
+ping -t 2 0 10 127.0.0.1 >nul
+goto rainbow
